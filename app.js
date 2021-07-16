4d5c1d0d663ec101ed78f5969aa8ed9e41994c73
tabishismail <tabish554@gmail.com>
2021-07-17 00:08:47 +0500

    styling and error fixed

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 
-let signUp = () =>{
+let signUp = () => {
     window.location = 'signup.html'
 }
-
-let allUser=[];
+let allUser = [];
 let saveUsers = localStorage.getItem('allUser');
-if(saveUsers){
-    allUser=JSON.parse(saveUsers);
-}else{
+if (saveUsers) {
+    allUser = JSON.parse(saveUsers);
+} else {
     allUser = [];
 }
-let auth=false;
-let store =()=>{
-    let fnSignUp=document.getElementById("fnSignUp");
-    let emailSignUp=document.getElementById("emailSignUp");
-    let userSignUp=document.getElementById("userSignUp");
-    let pswSignUp=document.getElementById("pswSignUp");
-    let storeUser={
-        fullname:fnSignUp.value,
-        email:emailSignUp.value,
-        username:userSignUp.value,
-        password:pswSignUp.value
+let store = () => {
+    let fnSignUp = document.getElementById("fnSignUp");
+    let emailSignUp = document.getElementById("emailSignUp");
+    let userSignUp = document.getElementById("userSignUp");
+    let pswSignUp = document.getElementById("pswSignUp");
+    let flag = false;
+    let storeUser = {
+        fullname: fnSignUp.value,
+        email: emailSignUp.value,
+        username: userSignUp.value,
+        password: pswSignUp.value
     }
-    for(var i=0 ;i < allUser.length; i++ ){
-        if(emailSignUp.value===allUser[i].email ){
-            alert("This email already exist");          
-        }
-        else if(userSignUp.value ===allUser[i].username){
-            alert("This User Name already exist");       
-        }
-        else{
-            allUser.push(storeUser);
-            localStorage.setItem('allUser',JSON.stringify(allUser));
-            window.location="index.html"
-        }
-        break
+    for (var i = 0; i < allUser.length; i++) {
+        if (emailSignUp.value=== allUser[i].email || userSignUp.value=== allUser[i].username ){
+            flag = true
+        }        
+    }
+    if(flag){
+        alert("This is already exist");
+    } else {
+        // console.log(allUser)
+        allUser.push(storeUser);
+        localStorage.setItem('allUser', JSON.stringify(allUser));
+        window.location='index.html';
     }
 }
